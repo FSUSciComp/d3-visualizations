3c2e11f132ced52ec60a364ed316f3ec42d813e9
drobinson6045 <davidr.tutoring@gmail.com>
2017-08-10 13:10:27 -0400

    Changed slider from N to delta X
    Added legends to plots
    Ensured dt and dx were divisors of t and L before calculation
    Changed L from 20 to 16 for better convergence
    Expanded explanation and included more latex
    Explanation of convergence plot will be added

--- robinson/Heat_Slider/d3FeBeSVG.js
+++ robinson/Heat_Slider/d3FeBeSVG.js
@@ -6,19 +6,27 @@
   var width = +svg.attr("width") - margin.left - margin.right,
       height = +svg.attr("height") - margin.top - margin.bottom;
  
-  var t_final = 2.0,
+  var xMin = -8,
+      xMax = 8;
+  var L = xMax - xMin;
+  var t_final = 1.0,
       n_Sol = 49,
-      dx_Sol = 20./(n_Sol-1);          
+      dx_Sol = +L/(n_Sol-1);          
   var Ns ={
       "nPrev" : n_Sol,
       "n" : n_Sol
+  };
+
+  var STEPS ={
+      "stepsPrev" : 1E3,
+      "steps" :  1E2 
   };
   var initialCond = [],
       solutionFinal = [];
 
   var xScale = d3.scaleLinear()
       .range([+margin.left,width-margin.right])
-      .domain([-10,10]);
+      .domain([xMin,xMax]);
   var yScale = d3.scaleLinear()
       .range([(+height-margin.bottom-100),margin.top])
       .domain([0,1]);
@@ -41,7 +49,7 @@
 
   //Build Initial Condition and Solution
   for(var i=0; i<n_Sol;i++){
-    var x = -10. +dx_Sol*i;
+    var x = xMin +dx_Sol*i;
     var yI = Solution(x,0);
     var yF = Solution(x,t_final);
     var tupleI = {
@@ -55,12 +63,11 @@
     initialCond.push(tupleI);
     solutionFinal.push(tupleF);
   }
-  console.log(solutionFinal[0].y); 
-
   var lineFormat = d3.line()
   .x(function(d){return xScale(d.x)})
   .y(function(d){return yScale(d.y)})
       .curve(d3.curveMonotoneX);
+
   //Plot IC and FS
   var IC = svg.append("path")
       .attr("class","Initial Condition")
@@ -126,10 +133,49 @@
         .attr("cy",function(d){return yScale(d.y)});
 
 //Draw Legend
-  
+  svg.append("g")
+      .attr("class","legend")
+      .append("path")
+      .attr("d",d3.line()([[175,75],[225,75]]))
+      .attr("stroke-width",3)
+      .attr("stroke","blue");
+  svg.select(".legend")
+    .append("circle")
+      .attr("r","7px")
+      .attr("fill","green")
+      .attr("cx",200)
+      .attr("cy",75);
+  svg.select(".legend")
+      .append("path")
+      .attr("d",d3.line()([[175,110],[225,110]]))
+      .attr("stroke-width",3)
+      .attr("stroke","red");
+  svg.select(".legend")
+    .append("circle")
+      .attr("r","5px")
+      .attr("cx",200)
+      .attr("cy",110);
+  svg.select(".legend")
+    .append("circle")
+      .attr("r","4px")
+      .attr("fill","green")
+      .attr("cx",200)
+      .attr("cy",110);
+  svg.select(".legend").append("text")
+      .attr("x",95)
+      .attr("y",78)
+      .attr("font-size","12px")
+      .style("font-weight","bold")
+      .text("Forward Euler");
+  svg.select(".legend").append("text")
+      .attr("x",89)
+      .attr("y",113)
+      .attr("font-size","12px")
+      .style("font-weight","bold")
+      .text("Backward Euler");
       
 //DT SLIDER
-  var dt_min = 1E-3,
+  var dt_min = 1E-5,
       dt_max = 1.0,
       dtFormat = d3.format(".1e");
   var sliderScale = d3.scaleLog()
@@ -163,7 +209,7 @@
       .attr("class","ticks")
       .attr("transform","translate(0,"+18+")")
     .selectAll("text")
-    .data(sliderScale.ticks(3))
+    .data(sliderScale.ticks(4))
     .enter().append("text")
       .attr("x",sliderScale)
       .attr("text-anchor","middle")
@@ -188,12 +234,21 @@
       .attr("font-size","12px")
       .style("font-weight","bold")
       .text("Timestep Size:");
+  //Move Slider Handle to 1E-2
+  dtHandle.attr("cx",sliderScale(t_final/STEPS.steps));
+  //Update Text
+  d3.selectAll(".dtText")
+    .attr("x",sliderScale(t_final/STEPS.steps)-16)
+    .text(dtFormat(t_final/STEPS.steps));
+  
 //DX slider
-  var nMax = 120,
+  var nMax = 200,
       nMin = 10,
-      dxFormat = d3.format("d");
-  var dxSliderScale = d3.scaleLinear()
-      .domain([nMin,nMax])
+      dxFormat = d3.format(".1e");
+  var dxMin = 1E-1,
+      dxMax = 1.0;
+  var dxSliderScale = d3.scaleLog()
+      .domain([dxMin,dxMax])
       .range([margin.left, width-margin.right])
       .clamp(true);
 
@@ -220,7 +275,7 @@
       .attr("class","ticks")
       .attr("transform","translate(0,"+18+")")
     .selectAll("text")
-    .data(dxSliderScale.ticks(11))
+    .data(dxSliderScale.ticks(1))
     .enter().append("text")
       .attr("x",dxSliderScale)
       .attr("text-anchor","middle")
@@ -240,17 +295,23 @@
       .attr("y", -10)
       .text(dxFormat(dxSliderScale.invert(dxpos)));
   dxHandleLabel.append("text")
-      .attr("x",-40)
+      .attr("x",-20)
       .attr("y",3)
-      .attr("font-size","10px")
+      .attr("font-size","14px")
       .style("font-weight","bold")
-      .text("Number of Points:");
+      .text("Delta x:");
   //Add text labels to plot
   var stabCond = svg.append("text")
       .attr("class","stabCond")
       .attr("transform","translate("+(width-margin.right-120)+","+(margin.top
               +20)+")")
       .text("Stability Condition: 0");
+  //Set initial DxSlider handle
+  dxHandle.attr("cx",dxSliderScale(L/(Ns.n-1)));
+  //Update Text
+  d3.selectAll(".dxText")
+    .attr("x",dxSliderScale(L/(Ns.n))-8)
+    .text(dxFormat(L/(Ns.n)));
 
     
   //Build LogLog Plot of Error
@@ -264,9 +325,9 @@
   var xLogScale = d3.scaleLog()
       .range([+margin.left,width-margin.right])
       //.domain([20./(nMax-1),20./(nMin-1)]);
-      .domain([1E-4,1]);
+      .domain([1E-1,1]);
   var yLogScale = d3.scaleLog()
-      .range([(+height-margin.bottom-100),margin.top])
+      .range([(+height-margin.bottom),margin.top])
       .domain([1E-4,1]);
   var xLAxis = d3.axisBottom(xLogScale),
       yLAxis = d3.axisLeft(yLogScale);
@@ -276,19 +337,100 @@
       .call(yLAxis);
   var xLAxisGroup = plotSvg.append("g")
       .attr("class","xLaxis")
-      .attr("transform","translate("+margin.left+"," + (height-margin.bottom-100) +
+      .attr("transform","translate("+margin.left+"," + (height-margin.bottom) +
           ")")//Shift down
       .call(xLAxis);
+  //Label static paths
+  svg.append("text")
+      .attr("text-anchor","middle")
+      .attr("y",100)
+      .attr("x",350)
+      .style("font-weight","bold")
+      .text("U(x,0)");
+  svg.append("text")
+      .attr("text-anchor","middle")
+      .attr("y",275)
+      .attr("x",500)
+      .style("font-weight","bold")
+      .attr("fill","green")
+      .text("U(x,1)");
   //Create group for error points
   plotSvg.append("g").attr("class","ErrorFE");
   plotSvg.append("g").attr("class","ErrorBE");
-
+//Plot line of slope 2
+  plotSvg.append("path")
+      .attr("d",d3.line()([[100,450],[500,250]]))
+      .attr("stroke-width",3)
+      .attr("stroke","blue");
+  plotSvg.append("text")
+      .attr("x",50)
+      .attr("y",470)
+      .attr("font-size","12px")
+      .style("font-weight","bold")
+      .attr("fill","blue")
+      .attr("transform","rotate(-25)")
+      .text("Slope = 2");
+  plotSvg.append("text")
+      .attr("x",-300)
+      .attr("y",50)
+      .attr("font-size","20px")
+      .style("font-weight","bold")
+      .attr("transform","rotate(-90)")
+      .text("Log(MaxError)");
+  plotSvg.append("text")
+      .attr("x",275)
+      .attr("y",500)
+      .attr("font-size","20px")
+      .style("font-weight","bold")
+      .text("Delta x");
+  plotSvg.append("text")
+      .attr("x",220)
+      .attr("y",50)
+      .attr("font-size","20px")
+      .style("font-weight","bold")
+      .text("Spatial Convergence Test");
+  plotSvg.append("circle")
+      .attr("r","4px")
+      .attr("fill","green")
+      .attr("cx",350)
+      .attr("cy",370);
+  plotSvg.append("text")
+      .attr("x",355)
+      .attr("y",373)
+      .attr("font-size","12px")
+      .style("font-weight","bold")
+      .attr("fill","green")
+      .text("Timestep = 1e-5s");
+  plotSvg.append("circle")
+      .attr("r","4px")
+      .attr("fill","red")
+      .attr("cx",350)
+      .attr("cy",400);
+  plotSvg.append("text")
+      .attr("x",355)
+      .attr("y",403)
+      .attr("font-size","12px")
+      .style("font-weight","bold")
+      .attr("fill","red")
+      .text("Timestep = 1.0s");
+/*
+//Draw borders around svg elements
+  //heat plot
+  DrawLine(0,0,800,0,svg); 
+  DrawLine(800,0,800,600,svg); 
+  DrawLine(0,600,800,600,svg); 
+  DrawLine(0,0,0,600,svg); 
+  //error plot
+  DrawLine(0,0,600,0,plotSvg); 
+  DrawLine(600,0,600,600,plotSvg); 
+  DrawLine(0,600,600,600,plotSvg); 
+  DrawLine(0,0,0,600,plotSvg); 
+*/
 //Plotting function
   function errorPlot(id,error,dt,dx){
     //ID: 0 for FE and 1 for BE
     if(id){
       //BE point plot
-      console.log(id,error,dt,dx);
       plotSvg.select(".ErrorBE")
           .append("circle")
             .attr("class","ErrorBEPoint")
@@ -317,43 +459,48 @@
 
   function dtSliding(h){
     dtHandle.attr("cx",sliderScale(h));
+    //Update Text
     d3.selectAll(".dtText")
       .attr("x",sliderScale(h)-16)
       .text(dtFormat(h));
-    calculateFE(); 
-
+    var roundSteps = Math.round(t_final/h);
+    if(roundSteps != STEPS.steps){
+      STEPS.stepsPrev = STEPS.steps;
+      STEPS.steps = roundSteps;
+      calculateFE(); 
+    }
   }
   function dxSliding(h){
     dxHandle.attr("cx",dxSliderScale(h));
+    //Update Text
     d3.selectAll(".dxText")
       .attr("x",dxSliderScale(h)-8)
       .text(dxFormat(h));
-  
-
+    //Make sure calculation doesn't occur for repeated N's
+    var roundedN = Math.round(L/h);
+    if(roundedN != Ns.n){ 
+      Ns.nPrev = Ns.n;
+      Ns.n = roundedN;
+      calculateFE();
+    }
   }
 
-  
+ //CHANGE THIS TO ADJUST FINAL DISPLAY VALUE 
   function updateDX(h){
     //Passed N value from slider(float)
-    var holder = Ns.n;
-    var roundedN = Math.round(h);
-    Ns.nPrev = holder;
-    Ns.n = roundedN;
     //Round Slider Value to Nearest Int (Snap)
-    dxHandle.attr("cx",dxSliderScale(roundedN));
-    calculateFE();
+    dxHandle.attr("cx",dxSliderScale(L/(Ns.n - 1)));
   }
 
   function calculateFE(){
     //Clear current function
-    var dt = sliderScale.invert(d3.selectAll(".dtHandle").attr("cx"));
-    var N =
-      Math.round(dxSliderScale.invert(d3.selectAll(".dxHandle").attr("cx")));
-    var dx = 20./(N-1);
+    var dt = t_final/STEPS.steps; 
+    var N = Ns.n;
+    var dx = L/(N-1);
     var constant = dt/dx/dx;
     var sol = [];
     for(var i=0; i<N; i++){ 
-      var x = -10. + dx*i;
+      var x = xMin + dx*i;
       var y = Solution(x,0);
       var tuple = {
         "x":x,
@@ -372,22 +519,16 @@
       }
       time += dt;
     }
-    var tDiff = t_final - time;
-    solP = sol.slice();
-    for(var i=1;i<N-1;i++){
-      sol[i].y= solP[i].y- tDiff/dx/dx*(2*solP[i].y-solP[i-1].y-solP[i+1].y);
-    }
-    time += tDiff;
     //Calculate max Error
     var maxFEerr = 0;
     if(constant<1){
       for(var i=1; i<N;i++){
-        var curErr = Math.abs(sol[i].y-Solution(sol[i].x,2.0));
+        var curErr = Math.abs(sol[i].y-Solution(sol[i].x,t_final));
         if(curErr > maxFEerr){
           maxFEerr=curErr;
         }
       }
-      errorPlot(0,maxFEerr,dt,dx);
+      //errorPlot(0,maxFEerr,dt,dx);
     }
     //Update Points
     time = 4*dt/dx/dx;
@@ -405,22 +546,21 @@
         .attr("r", "9px")
         .attr("cx",function(d){return xScale(d.x)+margin.left})
         .attr("cy",function(d){return yScale(d.y)})
-        .style("fill",function(d){return colorScale(Math.abs(d.y-Solution(d.x,2.0)))});
+        .style("fill",function(d){return colorScale(Math.abs(d.y-Solution(d.x,t_final)))});
     
     calculateBE();
   }
 
   function calculateBE(){
     //Clear current function
-    var dt = sliderScale.invert(d3.selectAll(".dtHandle").attr("cx"));
-    var N =
-      Math.round(dxSliderScale.invert(d3.selectAll(".dxHandle").attr("cx")));
-    var dx = 20./(N-1);
+    var dt = t_final/STEPS.steps; 
+    var N = Ns.n;
+    var dx = L/(N-1);
     var constant = dt/dx/dx;
     var constBE = 2 + dx*dx/dt;
     var solR =[];
     for(var i=0; i<N; i++){ 
-      var x = -10. + dx*i;
+      var x = xMin + dx*i;
       var y = Solution(x,0);
       var tuple = {
         "x":x,
@@ -450,29 +590,10 @@
       }
       time += dt;
     }
-    //Have to step for final time that's leftover from timestep size
-    var tDiff = t_final - time;
-    var delta = 5;
-    var b = solR.slice();
-    var constBE = 2 + dx*dx/tDiff;
-    var up = solR.slice();
-    var count = 0;
-    if (tDiff > 1E-6){
-      while(delta > 1E-6){
-        delta=0;
-        for(var k=1; k<N-1 ; k++){
-          solR[k].y = 1./constBE*(dx*dx/tDiff*b[k].y+solR[k-1].y+up[k+1].y);
-          delta += Math.abs(solR[k].y-up[k].y);
-        }
-        count += 1;
-        up = solR.slice();
-        if(count >5){break;}
-      }
-    }
     //Calculate Error
     var maxError=0;
     for(var i=0; i<N; i++){
-      curError = Math.abs(solR[i].y-Solution(solR[i].x,2.0));
+      curError = Math.abs(solR[i].y-Solution(solR[i].x,t_final));
       if(curError > maxError){
         maxError = curError;
       }
@@ -506,10 +627,16 @@
     svg.select(".pointsR").selectAll(".pointR").data(solR)
           .attr("cx",function(d){return xScale(d.x)+margin.left})
           .attr("cy",function(d){return yScale(d.y)})
-          .style("fill",function(d){return colorScale(Math.abs(d.y-Solution(d.x,2.0)))});
-    //errorPlot(1,maxError,dt,dx);
+          .style("fill",function(d){return colorScale(Math.abs(d.y-Solution(d.x,t_final)))});
+    errorPlot(1,maxError,dt,dx);
+  }
+  function DrawLine(x1,y1,x2,y2,canvas){
+    canvas.append("path")
+        .attr("d",d3.line()([[x1,y1],[x2,y2]]))
+        .attr("stroke","black")
+        .attr("stroke-width",6);
   }
 
   function Solution(x,t){
-    return 1./Math.pow(1.+4*t,0.5)*Math.exp(-x*x/(1+4*t));
+    return 1.0/Math.pow(1.+4*t,0.5)*Math.exp(-x*x/(1+4*t));
   }
